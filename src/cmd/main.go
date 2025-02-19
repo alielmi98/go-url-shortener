a989211c3ac55461086877c58f0c87b6d88e7104
Ali Elmi <ali.elmi77@yahoo.com>
2025-02-19 19:25:27 +0330

    initial config

---
+++ src/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/alielmi98/go-url-shortener/config"
+	"github.com/alielmi98/go-url-shortener/constants"
+	"github.com/alielmi98/go-url-shortener/data/cache"
+	"github.com/alielmi98/go-url-shortener/data/db"
+	"github.com/alielmi98/go-url-shortener/data/db/migrations"
+)
+
+func main() {
+	cfg := config.GetConfig()
+
+	err := cache.InitRedis(cfg)
+	defer cache.CloseRedis()
+	if err != nil {
+		log.Fatalf("caller:%s  Level:%s  Msg:%s", constants.Redis, constants.Startup, err.Error())
+	}
+
+	err = db.InitDb(cfg)
+	defer db.CloseDb()
+	if err != nil {
+		log.Fatalf("caller:%s  Level:%s  Msg:%s", constants.Postgres, constants.Startup, err.Error())
+	}
+
+	migrations.Up_1()
+}
---
+++ src/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	// User
+	DefaultUserName string = "admin"
+
+	// Claims
+	AuthorizationHeaderKey string = "Authorization"
+	UserIdKey              string = "UserId"
+	UsernameKey            string = "Username"
+	EmailKey               string = "Email"
+	ExpireTimeKey          string = "Exp"
+	RefreshTokenCookieName string = "refresh_token"
+)
