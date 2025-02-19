27955949d7498eff994d53db8aa43277268b3c54
Ali Elmi <ali.elmi77@yahoo.com>
2025-02-19 19:41:59 +0330

    API server initialization with Swagger documentation

---
+++ src/api/api.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/alielmi98/go-url-shortener/config"
+	"github.com/alielmi98/go-url-shortener/constants"
+	"github.com/alielmi98/go-url-shortener/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func InitServer(cfg *config.Config) {
+	r := gin.New()
+
+	RegisterRoutes(r, cfg)
+	RegisterSwagger(r, cfg)
+	log.Printf("Caller:%s Level:%s Msg:%s", constants.General, constants.Startup, "Started")
+	r.Run(fmt.Sprintf(":%s", cfg.Server.InternalPort))
+}
+
+func RegisterRoutes(r *gin.Engine, cfg *config.Config) {
+	//api := r.Group("/api")
+
+	//v1 := api.Group("/v1")
+	{
+		//User
+		//Short Url
+
+	}
+
+}
+func RegisterSwagger(r *gin.Engine, cfg *config.Config) {
+	docs.SwaggerInfo.Title = "golang web api"
+	docs.SwaggerInfo.Description = "golang URL Shortening Service api documentation"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api"
+	docs.SwaggerInfo.Host = fmt.Sprintf("localhost:%s", cfg.Server.InternalPort)
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
